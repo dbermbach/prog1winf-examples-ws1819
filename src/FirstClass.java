4f9dc90f5570045db3cef9a1143eb5e29f20e451
dbermbach <dbermbach@gmail.com>
2018-10-18 17:05:39 +0200

    add examples Oct 18

---
+++ src/FirstClass.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dave
+ */
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		int j;
+		j = 5;
+		
+		System.out.println(-j);
+		System.out.println(j);
+		
+		System.out.println(j++);
+		System.out.println(j);
+		
+		System.out.println(++j);
+		System.out.println(j);
+		
+		j = 123_456_789;
+		
+		long l = 5L;
+		
+		double d = 1.3;
+		d = .3;
+		d = 1.3e-15;
+
+		
+		char myChar = 'a';
+		
+		String str = "hello world sfhöheröiuhafökhasöfdh";
+		System.out.println(str);
+		myChar = '\'';
+		str = "\"\\";
+		System.out.println(str);
+	}
+
+}
